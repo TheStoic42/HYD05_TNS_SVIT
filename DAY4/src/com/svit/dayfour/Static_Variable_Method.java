bee9179e512b80f8ba67ade67462f5673d9def71
Prashanth <Prashanth@DESKTOP-KKKSSU2>
2023-10-23 22:41:22 +0530

    TNS HYD=05

---
+++ DAY4/src/com/svit/dayfour/Static_Variable_Method.java
@@ -0,0 +1,15 @@
+package com.svit.dayfour;
+
+public class Static_Variable_Method {
+	 public static void main(String[] args) {
+	        Object Cube = null;
+			System.out.println(Cube);
+	        System.out.println("Cube value of 5 is: "+Cube(5));
+	        
+	    }
+
+		private static String Cube(int i) {
+			// TODO Auto-generated method stub
+			return null;
+		}
+	}
