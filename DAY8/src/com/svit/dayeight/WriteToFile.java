ac3349faf56bb402a5b6c1f5c27f332d6c1667d9
TheStoic42 <132384701+TheStoic42@users.noreply.github.com>
2023-11-01 13:02:28 +0530

    Add files via upload

---
+++ DAY8/src/com/svit/dayeight/WriteToFile.java
@@ -0,0 +1,22 @@
+package com.svit.dayeight;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class WriteToFile {
+	 public static void main(String[] args) {
+			try {
+				
+			      FileWriter myWriter = new FileWriter("sample.txt");
+			      myWriter.write("This is a sample txt file created by using file handling operations!");
+			      myWriter.close();
+			      System.out.println("Successfully wrote to the file.");
+
+				
+			} catch (IOException e) {
+			      System.out.println("An error occurred.");
+			      e.printStackTrace();
+
+			}
+		}
+}
